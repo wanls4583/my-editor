4d75350ff4def9bfda10c3a38486c5fe431b55cf
我是新手 <409051987@qq.com>
2022-05-04 22:58:43 +0800

    [master]

--- public/process/lint/css.js
+++ public/process/lint/css.js
@@ -1,39 +1,14 @@
 const Stylelint = require('stylelint');
 
-process.on('uncaughtException', (e) => {
-    process.send('uncaughtException:', e);
-});
-
-process.on('message', (data) => {
-    let language = data.language.toLowerCase();
-    try {
-        switch (language) {
-            case 'css':
-            case 'scss':
-            case 'less':
-                lintCss(data, language);
-                break;
-        }
-    } catch (e) {
-        process.send({ error: e });
-    }
-});
-
-function lintCss(data, language) {
-    _lintCss(data, language).then((warnings)=>{
-        process.send({ parseId: data.parseId, results: warnings });
-    });
-}
-
-function _lintCss(data, language) {
+function lint(text, language) {
     const rules = {};
     const extend = [];
     if (language === 'css') {
         rules['max-nesting-depth'] = 0;
         extend.push('stylelint-config-css-modules');
     }
-    return new Promise((resolve)=>{
-        Stylelint.lint({ code: data.text, config: { rules: rules, extends: extend }, formatter: 'verbose' }).then(function (e) {
+    return new Promise((resolve) => {
+        Stylelint.lint({ code: text, config: { rules: rules, extends: extend }, formatter: 'verbose' }).then(function (e) {
             let results = e.results;
             let warnings = [];
             results.forEach((item) => {
@@ -53,9 +28,9 @@ function _lintCss(data, language) {
             });
             resolve(warnings);
         });
-    })
+    });
 }
 
-function lintHtml() {
-    
-}
+module.exports = {
+    lint: lint,
+};
---
+++ public/process/lint/html.js
@@ -0,0 +1,132 @@
+const Css = require('./css');
+
+const regs = {
+    javascript: /(?<!\<[^\>]*?['"][^\>]*?)(\<script(?:\s[^\>]*?)?\>)([\s\S]*?)\<\/script\>/,
+    css: /(?<!\<[^\>]*?['"][^\>]*?)(\<style(?:\s[^\>]*?)?\>)([\s\S]*?)\<\/style\>/,
+    enter: /\n/g,
+    column: /\n([^\n]+)$/,
+    comment: /\<\!--[\s\S]*?--\>/,
+};
+
+function parseComentRange(text) {
+    let exec = null;
+    let pos = {
+        line: 1,
+        column: 0,
+        index: 0,
+    };
+    let start = null;
+    let end = null;
+    let comments = [];
+    if (parseComentRange.cache && parseComentRange.cache.text === text) {
+        return parseComentRange.cache.comments;
+    }
+    while ((exec = regs.comment.exec(text))) {
+        setLineColumn(text.slice(0, exec.index), pos);
+        pos.index += exec.index;
+        start = Object.assign({}, pos);
+        setLineColumn(exec[0], pos);
+        pos.index += exec[0].length;
+        end = Object.assign({}, pos);
+        text = text.slice(pos.index);
+        comments.push({
+            start: start,
+            end: end,
+        });
+    }
+    parseComentRange.cache = {
+        text: text,
+        comments: comments,
+    };
+    return comments;
+}
+
+function setLineColumn(text, pos) {
+    let lines = text.match(regs.enter);
+    lines = (lines && lines.length) || 0;
+    pos.line += lines;
+    if (lines) {
+        let exec = regs.column.exec(text);
+        pos.column = (exec && exec[1].length) || 0;
+    } else {
+        pos.column += text.length;
+    }
+}
+
+function checkInComment(pos, comments) {
+    for (let i = 0; i < comments.length; i++) {
+        let item = comments[i];
+        if (_compare(item.start, pos) < 0 && _compare(item.end, pos) > 0) {
+            pos.line = item.end.line;
+            pos.column = item.end.column;
+            pos.index = item.end.index;
+            return false;
+        }
+    }
+    return true;
+
+    function _compare(a, b) {
+        if (a.line === b.line) {
+            return a.column - b.column;
+        }
+        return a.line - b.line;
+    }
+}
+
+function _lint(text, language) {
+    let exec = null;
+    let pos = {
+        line: 1,
+        column: 0,
+        index: 0,
+    };
+    let results = [];
+    let promises = [];
+    let comments = parseComentRange(text);
+    let reg = null;
+    let linter = null;
+    if (language === 'css') {
+        reg = regs.css;
+        linter = Css.lint;
+    }
+    if (language === 'javascript') {
+        reg = regs.javascript;
+    }
+    while ((exec = reg.exec(text))) {
+        setLineColumn(text.slice(0, exec.index), pos);
+        pos.index += exec.index;
+        if (!checkInComment(pos, comments)) {
+            text = text.slice(pos.index);
+            continue;
+        }
+        setLineColumn(exec[1], pos);
+        pos.index += exec[1].length;
+        if (exec[2]) {
+            let _pos = Object.assign({}, pos);
+            let promise = linter(exec[2], language).then((errors) => {
+                errors.forEach((item) => {
+                    if (item.line == 1) {
+                        item.column += _pos.column;
+                    }
+                    item.line += _pos.line - 1;
+                });
+                results.push(...errors);
+            });
+            promises.push(promise);
+        }
+        setLineColumn(exec[0].slice(exec[1].length), pos);
+        pos.index += exec[0].slice(exec[1].length).length;
+        text = text.slice(pos.index);
+    }
+    return Promise.all(promises).then(() => {
+        return results;
+    });
+}
+
+function lint(text) {
+    return _lint(text, 'css');
+}
+
+module.exports = {
+    lint: lint,
+};
