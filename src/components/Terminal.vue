6c33b5fdb892d0429d7e63d6c16031af57debea8
我是新手 <409051987@qq.com>
2022-05-14 18:13:56 +0800

    [master]

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -4,22 +4,24 @@
  * @Description: 
 -->
 <template>
-	<div @click="onClickTerminal" @contextmenu.stop.prevent="onContextmenu" class="my-terminal my-scroll-overlay my-scroll-small">
-		<div v-for="(item, index) in list">
-			<span>{{item.text || '&nbsp;'}}</span>
-			<template v-if="index === list.length - 1">
-				<span>{{text}}</span>
-				<span :style="{opacity: opacity}" class="my-terminal-cursor">
-					<textarea @blur="onBlur" @focus="onFocus" @keydown="onKeydown" class="my-terminal-textarea" ref="textarea" v-model="text"></textarea>
-				</span>
+	<div class="my-terminal" ref="terminal">
+		<div :style="{'padding-bottom': _lineHeight}" @click="onClickTerminal" @contextmenu.stop.prevent="onContextmenu" @scroll="onScroll" class="my-height-100 my-scroll-overlay my-scroll-small">
+			<template v-for="(item, index) in list">
+				<span :style="{'line-height': _lineHeight}" class="my-terminal-line">{{item.text || '&nbsp;'}}</span>
+				<br v-if="index < list.length - 1" />
 			</template>
+			<span class="my-terminal-line">{{text}}</span>
+			<span :style="{opacity: opacity}" class="my-terminal-cursor" ref="cursor"></span>
 		</div>
-		<template v-if="!list.length">
-			<span>{{text}}</span>
-			<span :style="{opacity: opacity}" class="my-terminal-cursor">
-				<textarea @blur="onBlur" @focus="onFocus" @keydown="onKeydown" class="my-terminal-textarea" ref="textarea" v-model="text"></textarea>
-			</span>
-		</template>
+		<textarea
+			:style="{left: textareaPos.left + 'px', top: textareaPos.top + 'px', height: _lineHeight, 'line-height': _lineHeight }"
+			@blur="onBlur"
+			@focus="onFocus"
+			@keydown="onKeydown"
+			class="my-terminal-textarea"
+			ref="textarea"
+			v-model="text"
+		></textarea>
 	</div>
 </template>
 <script>
@@ -32,7 +34,19 @@ export default {
 			list: [],
 			opacity: 0,
 			text: '',
+			lineHeight: 21,
+			scrollLeft: 0,
+			scrollTop: 0,
+			textareaPos: {
+				left: 0,
+				top: 0,
+			},
 		};
+	},
+	computed: {
+		_lineHeight() {
+			return this.lineHeight + 'px';
+		},
 	},
 	watch: {
 		text() {
@@ -54,7 +68,16 @@ export default {
 			this.addLine(data);
 		});
 	},
+	mounted() {
+		this.initResizeEvent();
+	},
 	methods: {
+		initResizeEvent() {
+			const resizeObserver = new ResizeObserver((entries) => {
+				this.renderCursor();
+			});
+			resizeObserver.observe(this.$refs.terminal);
+		},
 		addLine(data) {
 			let texts = iconvLite.decode(data, 'cp936').split(/\r\n|\n|\r/);
 			texts = texts.map((item) => {
@@ -70,7 +93,8 @@ export default {
 			this.list.push(...texts);
 			this.added = true;
 			requestAnimationFrame(() => {
-				this.$refs.textarea[0].scrollIntoView({ behavior: 'instant' }); //"auto","instant"或"smooth"
+				this.$refs.cursor.scrollIntoView();
+				this.renderCursor();
 				if (this.added) {
 					setTimeout(() => {
 						this.focus();
@@ -100,10 +124,26 @@ export default {
 			this.cursorVisible = false;
 			this.opacity = 0;
 		},
+		renderCursor() {
+			this.$nextTick(() => {
+				let terminal = this.$refs.terminal;
+				let height = terminal.clientHeight;
+				let cursor = this.$refs.cursor;
+				let left = cursor.offsetLeft - this.scrollLeft;
+				let top = cursor.offsetTop - this.scrollTop;
+				left = left < 0 ? 0 : left;
+				top = top > height - this.lineHeight ? height - this.lineHeight : top;
+				top = top < 0 ? 0 : top;
+				this.textareaPos = {
+					left: left,
+					top: top,
+				};
+			});
+		},
 		focus() {
-			this.$refs.textarea[0].focus();
+			this.$refs.textarea.focus();
 			requestAnimationFrame(() => {
-				this.$refs.textarea[0].focus();
+				this.$refs.textarea.focus();
 			});
 		},
 		onFocus() {
@@ -122,6 +162,11 @@ export default {
 			this.focus();
 		},
 		onContextmenu(e) {},
+		onScroll(e) {
+			this.scrollTop = e.target.scrollTop;
+			this.scrollLeft = e.target.scrollLeft;
+			this.renderCursor();
+		},
 	},
 };
 </script>
