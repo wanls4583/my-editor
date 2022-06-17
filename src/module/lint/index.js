ea2819ac7335e9e54cfd646376c4f337c329a33b
我是新手 <409051987@qq.com>
2022-06-17 11:40:14 +0800

    [master]

--- src/module/lint/index.js
+++ src/module/lint/index.js
@@ -10,6 +10,34 @@ const path = window.require('path');
 const child_process = window.require('child_process');
 
 let worker = null;
+let parseIdMap = {};
+
+function inttWorker() {
+	if (worker === null) {
+		clearTimeout(inttWorker.initWorkTimer);
+		inttWorker.initWorkTimer = setTimeout(() => {
+			worker = child_process.fork(path.join(globalData.dirname, 'main/process/lint/index.js'));
+			_bindMsg();
+		}, 300);
+	} else {
+		_bindMsg();
+	}
+
+	function _bindMsg() {
+		worker.on('message', data => {
+			if (typeof parseIdMap[data.parseId] === 'function') {
+				delete parseIdMap[data.parseId];
+				parseIdMap[data.parseId](data.results);
+			}
+		});
+		worker.on('close', () => {
+			worker = null;
+			inttWorker();
+		});
+	}
+}
+
+inttWorker();
 
 export default class {
 	constructor(editor, context) {
@@ -26,42 +54,14 @@ export default class {
 		}
 		this.language = language;
 		this.setErrors([]);
-		this.initEvent();
 		this.parse();
 	}
-	initEvent() {
-		if (!this.language) {
-			return;
-		}
-		if (worker === null) {
-			clearTimeout(this.initWorkTimer);
-			this.initWorkTimer = setTimeout(() => {
-				worker = child_process.fork(path.join(globalData.dirname, 'main/process/lint/index.js'));
-				_bindMsg.call(this);
-			}, 300);
-		} else {
-			_bindMsg.call(this);
-		}
-
-		function _bindMsg() {
-			worker.on('message', data => {
-				console.log(data);
-				if (data.parseId === this.parseId) {
-					this.setErrors(data.results);
-				}
-			});
-			worker.on('close', () => {
-				worker = null;
-				this.initEvent();
-			});
-		}
-	}
 	onInsertContentAfter(nowLine, newLine) {
-		this.parseId = '';
+		this.parseId && delete parseIdMap[this.parseId];
 		this.parse();
 	}
 	onDeleteContentAfter(nowLine, newLine) {
-		this.parseId = '';
+		this.parseId && delete parseIdMap[this.parseId];
 		this.parse();
 	}
 	parse() {
@@ -72,6 +72,9 @@ export default class {
 		this.parseTimer = setTimeout(() => {
 			const text = this.getAllText();
 			this.parseId = Util.getUUID();
+			parseIdMap[this.parseId] = results => {
+				this.setErrors(results);
+			};
 			worker.send({
 				text: text,
 				parseId: this.parseId,
