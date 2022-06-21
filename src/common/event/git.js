3e6a155558d8ed00abe94cb8af2bebadde98a2fc
我是新手 <409051987@qq.com>
2022-06-21 18:46:04 +0800

    [master-use jest-diff]

--- src/common/event/git.js
+++ src/common/event/git.js
@@ -1,12 +1,12 @@
-import Btree from '@/common/btree';
 import EventBus from '@/event';
 import globalData from '@/data/globalData';
 import Util from '../util';
 import Context from '@/module/context/index';
+import { diffLinesRaw } from 'jest-diff';
 
-const fs = window.require('fs');
+const spawnSync = window.require('child_process').spawnSync;
+const spawn = window.require('child_process').spawn;
 const path = window.require('path');
-const child_process = window.require('child_process');
 const SimpleGit = window.require('simple-git');
 const contexts = Context.contexts;
 
@@ -16,7 +16,9 @@ export default class {
 		this.simpleGit = new SimpleGit();
 		this.gitStatusTimer = {};
 		this.gitDiffTimer = {};
-		this.diffProcessMap = {};
+		this.maxCacheSize = 100 * 10000 * 20;
+		this.cacheIndexs = [];
+		this.fileCacheMap = { size: 0 };
 		this.init();
 	}
 	init() {
@@ -31,106 +33,123 @@ export default class {
 			this.gitDiffTimer[filePath] = setTimeout(() => {
 				let tab = filePath && Util.getTabByPath(globalData.editorList, filePath);
 				if (tab && tab.active && filePath) {
-					this.diffProcessMap[filePath] && this.diffProcessMap[filePath].kill();
-					this.diffProcessMap[filePath] = this.createDiffProcess(filePath);
-					this.diffProcessMap[filePath].send({ path: filePath, content: contexts[tab.id].getAllText() });
+					this.getDiff(
+						filePath,
+						contexts[tab.id].htmls.map(item => {
+							return item.text;
+						})
+					);
 				}
 			}, 500);
 		});
 	}
-	createDiffProcess(filePath) {
-		let diffProcess = child_process.fork(path.join(globalData.dirname, 'main/process/git/diff.js'), { cwd: path.dirname(filePath), timeout: 10000 });
-		diffProcess.on('message', data => {
-			this.diffProcessMap[filePath] = null;
-			diffProcess.kill();
-			EventBus.$emit('git-diffed', { path: data.path, result: this.parseDiff(data.result) });
-		});
-		return diffProcess;
-	}
-	parseDiff(data) {
-		let result = [];
-		while (data.length) {
-			let item = data.shift();
-			item.value = item.value.split('\n');
-			if (item.removed) {
-				if (data.length && data[0].added && data[0].line === item.line) {
-					let next = data.shift();
-					next.value = next.value.split('\n');
-					if (next.value.length) {
-						result.push(
-							_createRange({
-								type: 'M',
-								line: item.line,
-								deleted: item.value,
-								added: next.value.slice(0, item.value.length),
-							})
-						);
-						if (next.value.length > item.value.length) {
-							result.push(
-								_createRange({
-									type: 'A',
-									line: next.line + item.value.length,
-									added: next.value.slice(item.value.length),
-								})
-							);
-						}
-					} else {
-						result.push(
-							_createRange({
-								type: 'D',
-								line: item.line,
-								deleted: item.value,
-							})
-						);
-					}
-				} else {
-					!item.end && item.value.pop();
-					result.push(
-						_createRange({
-							type: 'D',
-							line: item.line,
-							deleted: item.value,
-						})
-					);
-				}
-			} else {
-				!item.end && item.value.pop();
-				result.push(
-					_createRange({
-						type: 'A',
-						line: item.line,
-						added: item.value,
-					})
-				);
-			}
+	getDiff(filePath, textArr) {
+		let nowCommit = this.getNowHash(filePath);
+		let fileIndex = nowCommit + '-' + this.getCacheHash(filePath);
+		let stagedContent = this.fileCacheMap[fileIndex];
+		if (!nowCommit) {
+			return;
 		}
-		return result;
+		if (!stagedContent) {
+			this.getStagedContent(filePath).then(stagedContent => {
+				this.cacheFile(fileIndex, stagedContent);
+				_diff.call(this, stagedContent);
+			});
+		} else {
+			_diff.call(this, stagedContent);
+		}
 
-		function _createRange({ type, line, added, deleted }) {
-			added = (added || []).slice();
-			deleted = (deleted || []).slice();
-			while (added.peek() === '' && deleted.peek() === '') {
-				added.pop();
-				deleted.pop();
-			}
-			while (added[0] === deleted[0]) {
-				added.shift();
-				deleted.shift();
+		function _diff(text) {
+			let diffs = diffLinesRaw(text.split('\n'), textArr);
+			EventBus.$emit('git-diffed', { path: filePath, result: this.parseDiff(diffs) });
+		}
+	}
+	parseDiff(diffs) {
+		let results = [];
+		let diffObjs = [];
+		let preDiffObj = null;
+		let line = 1;
+		for (let i = 0; i < diffs.length; i++) {
+			let item = diffs[i];
+			if (item[0] === 0) {
+				//common
+				line++;
+				preDiffObj = null;
+			} else if (item[0] === -1) {
+				//delete
+				if (preDiffObj && preDiffObj.type === 'D' && preDiffObj.line === line) {
+					preDiffObj.deleted.push(item[1]);
+				} else {
+					preDiffObj = {
+						type: 'D',
+						line: line,
+						added: [],
+						deleted: [item[1]],
+					};
+					diffObjs.push(preDiffObj);
+				}
+			} else if (item[0] === 1) {
+				//add
+				if (preDiffObj && preDiffObj.type === 'A' && preDiffObj.line === line - 1) {
+					preDiffObj.added.push(item[1]);
+				} else {
+					preDiffObj = {
+						type: 'A',
+						line: line,
+						added: [item[1]],
+						deleted: [],
+					};
+					diffObjs.push(preDiffObj);
+				}
 				line++;
 			}
-			// if (added.length === 1 && added[0] === '') {
-			// 	added = [];
-			// }
-			if (!added.length) {
-				type = 'D';
-			}
-			return { type, line, added, deleted };
 		}
+		for (let i = 0; i < diffObjs.length; i++) {
+			let item = diffObjs[i];
+			let nextItem = diffObjs[i + 1];
+			if (item.type === 'D' && nextItem && nextItem.type === 'A' && nextItem.line === item.line) {
+				if (item.deleted.length === nextItem.added.length) {
+					results.push({
+						type: 'M',
+						line: item.line,
+						added: nextItem.added,
+						deleted: item.deleted,
+					});
+				} else if (item.deleted.length > nextItem.added.length) {
+					results.push({
+						type: 'M',
+						line: item.line,
+						added: nextItem.added,
+						deleted: item.deleted.slice(0, nextItem.added.length),
+					});
+					results.push({
+						type: 'D',
+						line: item.line + nextItem.added.length,
+						added: [],
+						deleted: item.deleted.slice(nextItem.added.length),
+					});
+				} else {
+					results.push({
+						type: 'M',
+						line: item.line,
+						added: nextItem.added.slice(0, item.deleted.length),
+						deleted: item.deleted,
+					});
+					results.push({
+						type: 'A',
+						line: item.line + item.deleted.length,
+						added: nextItem.added.slice(item.deleted.length),
+						deleted: [],
+					});
+				}
+				i++;
+			} else {
+				results.push(item);
+			}
+		}
+		return results;
 	}
 	gitStatus(fileObj) {
-		// if (!fs.existsSync(path.join(fileObj.path, '.git'))) {
-		// 	return;
-		// }
 		if (this.cwd != fileObj.path) {
 			this.simpleGit.cwd(fileObj.path);
 		}
@@ -199,5 +218,47 @@ export default class {
 				filePath = path.dirname(filePath);
 			}
 		}
+	}
+	cacheFile(fileIndex, stagedContent) {
+		if (this.fileCacheMap.size + stagedContent.length > this.maxCacheSize) {
+			let max = this.maxCacheSize - stagedContent.length;
+			while (this.cacheIndexs.length && this.fileCacheMap.size > max) {
+				let index = this.cacheIndexs.shift();
+				this.fileCacheMap.size -= this.fileCacheMap[index];
+				delete this.fileCacheMap[index];
+			}
+		}
+		this.fileCacheMap[fileIndex] = stagedContent;
+		this.fileCacheMap.size += stagedContent.length;
+	}
+	getNowHash(filePath) {
+		return spawnSync('git', ['log', '-1', '--format=%H'], { cwd: path.dirname(filePath) }).stdout.toString();
+	}
+	getCacheHash(filePath) {
+		let fileIndex = '';
+		try {
+			fileIndex = spawnSync('git', ['diff-index', 'HEAD', '--cached', filePath], { cwd: path.dirname(filePath) });
+			fileIndex = fileIndex.stdout.toString() || filePath;
+		} catch (e) {
+			console.log(e);
+		}
+		return fileIndex;
+	}
+	getStagedContent(filePath) {
+		let child = null;
+		if (process.platform === 'win32') {
+			child = spawn('cmd', [`/C chcp 65001>nul && git show :./${path.basename(filePath)}`], { cwd: path.dirname(filePath) });
+		} else {
+			child = spawn('git', ['show', `:./${path.basename(filePath)}`], { cwd: path.dirname(filePath) });
+		}
+		return new Promise(resolve => {
+			let result = '';
+			child.stdout.on('data', data => {
+				result += data;
+			});
+			child.on('close', () => {
+				resolve(result);
+			});
+		});
 	}
 }
