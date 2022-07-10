3485e48d8938c51e200c87c9db508274dfde6e71
我是新手 <409051987@qq.com>
2022-07-10 16:08:52 +0800

    [nw]

--- src/main.js
+++ src/main.js
@@ -1,19 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
-import axios from 'axios';
 import 'xterm/css/xterm.css';
 import './scss/index.scss';
 
 window.require = window.require || function () {};
 
 Vue.config.productionTip = false;
-Vue.prototype.$http = axios.create({
-	timeout: 1000 * 30,
-	withCredentials: true,
-	headers: {
-		'Content-Type': 'application/json; charset=utf-8',
-	},
-});
 
 new Vue({
 	render: h => h(App),
