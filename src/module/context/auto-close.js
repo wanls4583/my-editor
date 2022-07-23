522088968dc3c57ec927f921e860541fc7d5bfa9
我是新手 <409051987@qq.com>
2022-07-24 00:36:15 +0800

    [master]-checkClose

--- src/module/context/auto-close.js
+++ src/module/context/auto-close.js
@@ -6,6 +6,9 @@ export default class {
         this.context = context;
     }
     check(text) {
+        return this.checkOpen(text) || this.checkClose(text);
+    }
+    checkOpen(text) {
         if (text.length > 1 || this.editor.selecter.activedRanges.size) {
             return false;
         }
@@ -13,18 +16,19 @@ export default class {
         let cursorPos = null;
         let sourceConfigData = null;
         let pass = false;
-        let _text = '';
+        let beforText = '';
         let texts = [];
         let cursorPosList = [];
         let head = this.editor.cursor.multiCursorPos.getHead();
         while (cursorPos = head.next()) {
             pass = false;
-            _text = this.context.htmls[cursorPos.line - 1].text.slice(0, cursorPos.column) + text;
+            beforText = this.context.htmls[cursorPos.line - 1].text.slice(0, cursorPos.column) + text;
             sourceConfigData = this.context.getConfigData(cursorPos);
             if (sourceConfigData) {
                 for (let i = 0; i < sourceConfigData.autoClosingPairs.length; i++) {
-                    if (_text.endsWith(sourceConfigData.autoClosingPairs[i].open)) {
-                        texts.push(text + sourceConfigData.autoClosingPairs[i].close);
+                    let item = sourceConfigData.autoClosingPairs[i];
+                    if (beforText.endsWith(item.open)) {
+                        texts.push(text + item.close);
                         pass = true;
                         break;
                     }
@@ -48,6 +52,50 @@ export default class {
         historyArr.afterCursorPosList = afterCursorPosList;
         this.context.addCursorList(afterCursorPosList);
         this.editor.history.pushHistory(historyArr);
+        return true;
+    }
+    checkClose(text) {
+        if (text.length > 1 || this.editor.selecter.activedRanges.size) {
+            return false;
+        }
+        let nowWord = null;
+        let cursorPos = null;
+        let sourceConfigData = null;
+        let pass = false;
+        let close = '';
+        let beforText = '';
+        let afterText = '';
+        let cursorPosList = [];
+        let head = this.editor.cursor.multiCursorPos.getHead();
+        while (cursorPos = head.next()) {
+            pass = false;
+            beforText = this.context.htmls[cursorPos.line - 1].text;
+            afterText = beforText.slice(cursorPos.column);
+            beforText = beforText.slice(0, cursorPos.column) + text;
+            sourceConfigData = this.context.getConfigData(cursorPos);
+            if (sourceConfigData) {
+                for (let i = 0; i < sourceConfigData.autoClosingPairs.length; i++) {
+                    let item = sourceConfigData.autoClosingPairs[i];
+                    if (beforText.endsWith(item.close)) {
+                        if (afterText.startsWith(item.close)) {
+                            close = item.close;
+                            pass = true;
+                        }
+                        break;
+                    }
+                }
+            }
+            if (!pass) {
+                return false;
+            }
+            nowWord = this.editor.searcher.getNowWord(cursorPos);
+            if (nowWord.text && Util.comparePos(nowWord.range.end, cursorPos) !== 0) {
+                return false;
+            }
+            cursorPosList.push({ line: cursorPos.line, column: cursorPos.column + close.length });
+        }
+        this.editor.cursor.clearCursorPos();
+        this.context.addCursorList(cursorPosList);
         return true;
     }
 }
